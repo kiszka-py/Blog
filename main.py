0c162ca212e03ce5d3f22f92bf5f6bec8b98e931
Krzysztof <krzysztofkiszka@yahoo.com>
2022-08-20 17:34:02 +0200

    Heroku.

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ from wtforms import StringField, validators, PasswordField, SubmitField
 from functools import wraps
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
